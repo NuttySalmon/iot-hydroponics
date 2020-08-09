1e13cf8369ffddcd2b2ef8bc04f14c7b35be56fd
NuttySalmon <nuttysalmon@gmail.com>
2020-08-08 18:28:29 -0700

    Added sample sass and basic routing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import Login from './Login';
+import Landing from './Landing';
+import SignUp from './SignUp';
+import UISample from './common/UISample';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/sample">
+          <UISample />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ react-app/src/common/UISample.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './scss/components/logos.scss';
+import './scss/components/form.scss'
+import { Form, Button, Container } from 'react-bootstrap';
+import TextField from './components/TextField';
+
+function UISample() {
+  return (
+    <div style={{padding: '30px'}}>
+      <h1 className="logo-lg">TitleLogo</h1>
+      <h1>Header</h1>
+      <p>Paragraph</p>
+      <Button variant='short'> Button </Button>
+      <Button variant='long'> Long </Button>
+      <div style={{ background: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("https://post.healthline.com/wp-content/uploads/2020/03/taro-leaves-leaf-732x549-thumbnail.jpg")', padding: '10px', width:'100vw', marginTop: '50px'}}>
+        <h3 className='logo-trans'> TransLogo</h3>
+        <Button variant="trans">Transparent</Button>
+        <Container style={{backgroundColor: 'white', margin:'30px', padding: '10px'}}>
+          <Form>
+            <TextField />
+          </Form>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default UISample;
