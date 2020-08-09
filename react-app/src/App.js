4e87ca58511da463ec307c0d3deaae24436fb17b
NuttySalmon <nuttysalmon@gmail.com>
2020-08-08 18:46:09 -0700

    Added basic links for navigation

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,20 @@ import './App.scss';
 function App() {
   return (
     <Router>
+      <ul>
+        <li>
+          <Link to="/">Landing</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/sign-up">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/sample/">SASS Sample</Link>
+        </li>
+      </ul>
       <Switch>
         <Route path="/sample">
           <UISample />
