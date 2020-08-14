4dd18ed38105bb09a36daa3b2fd03ba6968bdb3c
Jon-Jon <j2naraja@gmail.com>
2020-08-13 20:49:28 -0700

    Included footer, changed color of buttons to green when hovered

--- mockup/landing-page/landing-page-react/src/App.js
+++ mockup/landing-page/landing-page-react/src/App.js
@@ -5,6 +5,10 @@ import Hydroponics from "./Hydroponics";
 import EbbAndFlow from "./EbbAndFlow";
 import WhyHydro from "./WhyHydro";
 import './style.css';
+import { 
+  Container,
+  Button 
+} from "react-bootstrap";
 class App extends Component {
   render() {
     return (
@@ -14,6 +18,15 @@ class App extends Component {
         <Hydroponics />
         <EbbAndFlow />
         <WhyHydro />
+        <footer style={{backgroundColor: "#5D4C09"}}>
+          <Container fluid>
+            <div className="col-12 text-center">
+              <h5 style={{color: "white"}}>
+                Copyright © 2020 HydroGarden. All right reserved. | <Button variant="btn btn-foot" style={{color: "white"}}>Terms of Use</Button> {' '} | <Button variant="btn btn-foot" style={{color: "white"}}>Privacy Policy</Button>
+              </h5>
+            </div>
+          </Container>
+        </footer>
       </div>
       
     );
