98a9b9e5003c6640858fc092a88814fce551dba1
NuttySalmon <nuttysalmon@gmail.com>
2020-08-15 17:24:38 -0700

    Got values in login form

--- react-app/src/Login/LoginForm.js
+++ react-app/src/Login/LoginForm.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, {createRef} from 'react'
 import {Form, Button, Row, Col} from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+// import { Auth } from 'aws-amplify';
 import TextField from '../common/components/TextField'
 import '../common/scss/components/buttons.scss';
 import '../common/scss/components/form-elements.scss';
 import style from './scss/login.module.scss'
 
+
 const LoginForm = () => {
+  const emailField = createRef();
+  const passwordField = createRef(); 
+
+  const awsSignIn = async () => {
+      const username = emailField.current.value
+      const password = passwordField.current.value
+      try {
+          // const user = await Auth.signIn(username, password);
+          // console.log(user);
+      } catch (error) {
+          console.log('error signing in', error);
+      }
+  }
   return (
     <Form className='user-form'>
-      <TextField type="email"> Email </TextField>
-      <div style={{ marginTop: "2.5em" }}><TextField type="password">Password</TextField></div>
-      <Row><Col className={style.formBtn}><Button variant="short"> Login </Button></Col></Row>
+      <TextField ref={emailField} type="email"> Email </TextField>
+      <div style={{ marginTop: "2.5em" }}><TextField ref={passwordField} type="password">Password</TextField></div>
+      <Row><Col className={style.formBtn}><Button variant="short" onClick={awsSignIn}> Login </Button></Col></Row>
       <div className={style.formLink}> 
         <Row><Col><Link to='/signup'>Don&apos;t have an account? </Link></Col></Row>
         <Row className="mt-1"><Col><Link to='/forget-password'>Forget password</Link></Col></Row>
--- react-app/src/common/components/TextField.js
+++ react-app/src/common/components/TextField.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 import { Form } from 'react-bootstrap';
 import '../scss/components/form-elements.scss'
 
-const TextField = ({children, id, type='text', as=undefined}) => {
+const TextField = React.forwardRef(({children, id, type='text', as=undefined}, ref) => {
   const [empty, changeEmptyState] = useState(true);
   const handleInput = (e) => changeEmptyState(e.target.value.length === 0);
   return (
     <Form.Group as={as} controlId={id}>
       <Form.Label className={empty ? 'label-center': 'label-small'}>{children}</Form.Label>
-      <Form.Control type={type} onChange={handleInput} />
+      <Form.Control ref={ref} type={type} onChange={handleInput} />
     </Form.Group>
   );
-}
+});
 
 export default TextField
