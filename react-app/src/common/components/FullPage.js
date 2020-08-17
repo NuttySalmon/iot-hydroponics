50d728340b5980b3d5aa40adff1e884b49b53b13
NuttySalmon <nuttysalmon@gmail.com>
2020-08-16 23:48:22 -0700

    Made logo move right when windows too short on sign up.

--- react-app/src/common/components/FullPage.js
+++ react-app/src/common/components/FullPage.js
@@ -1,33 +1,39 @@
 import React from 'react'
-import {Row} from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import LogoTrans from './LogoTrans'
 import '../scss/components/form-elements.scss'
 
 /**
- * Render page with full background and logo 
+ * Render page with full background and logo
  * @param props
- * @param props.children - Element to render in page 
+ * @param props.children - Element to render in page
  * @param props.bg - background image
+ * @param props.rightLogo - move logo to right when windows too short
  */
-const FullPage = ({children, bg}) => {
+
+const FullPage = ({ children, bg, rightLogo }) => {
   return (
-    <div className='full-bg' style={{backgroundImage: `url(${bg})`}}> 
+    <div
+      className={`full-bg ${rightLogo ? 'logo-right-short' : ''}`}
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <LogoTrans />
-      <Row style={{margin: 0, height: '100%', width: '100vw'}}>
+      <Row style={{ margin: 0, height: '100%', width: '100vw' }}>
         {children}
       </Row>
     </div>
-  )
-}
+  );
+};
 
 FullPage.propTypes = {
-  bg: PropTypes.string
-}
+  bg: PropTypes.string,
+  rightLogo: PropTypes.bool,
+};
 
-FullPage.defaultProps ={
-  bg: null 
-}
+FullPage.defaultProps = {
+  bg: null,
+  rightLogo: false,
+};
 
-
-export default FullPage
+export default FullPage;
