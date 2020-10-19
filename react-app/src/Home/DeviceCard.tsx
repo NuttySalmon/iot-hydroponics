6868bcb0cd62737e6ae4339709a513ebf56a6425
Jon-Jon <j2naraja@gmail.com>
2020-10-19 14:51:13 -0700

    Displayed data on web app

--- react-app/src/Home/DeviceCard.tsx
+++ react-app/src/Home/DeviceCard.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+type DeviceCardProps = {
+  name: string
+  currentData: {
+    updatedAt: string
+    hum: string
+    temp: string
+    valveOpen: boolean
+  }
+}
 
-function DeviceCard() {
+function DeviceCard({ name, currentData }: DeviceCardProps) {
   return (
     <Row>
       <Col>
-        <p> Test From DeviceCard </p>
+        <p> {name} </p>
+        <p> {currentData?.updatedAt}</p>
+        <p> {currentData?.hum}</p>
+        <p> {currentData?.temp}</p>
+        {/* <p>Draining: {currentData?.valveOpen}</p> */}
       </Col>
     </Row>
   )
