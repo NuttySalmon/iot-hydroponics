855e6e282579bd25ef4de951bae33e8cf368c2dd
NuttySalmon <nuttysalmon@gmail.com>
2020-11-02 03:45:56 -0800

    Fixed auth routing for ready.

--- react-app/src/AuthPages/Logout.tsx
+++ react-app/src/AuthPages/Logout.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react'
 import { Auth } from 'aws-amplify'
+import { useHistory } from 'react-router-dom'
 
 const Logout = () => {
+  const history = useHistory()
   // Function to sign out the user
   async function signOut() {
     try {
       const user = await Auth.signOut()
       console.log(user)
       console.log('Logged out.')
+      history.push('/login')
     } catch (error) {
       console.log('error signing out: ', error)
     }
