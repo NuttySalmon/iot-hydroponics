583b075fa98b1d9a8d993ea26aad38dd00e814eb
NuttySalmon <nuttysalmon@gmail.com>
2020-11-02 13:57:58 -0800

    Added home page design

--- react-app/src/TempUI/Home.tsx
+++ react-app/src/TempUI/Home.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import DevCard from './DevCard'
+import style from './home.module.scss'
 
 const Home = () => {
-  return <div>Dashboard</div>
+  const cards = []
+  for (let i = 0; i < 5; i += 1) {
+    cards.push(
+      <DevCard
+        className={style.devCard}
+        name={i.toString()}
+        temp={123}
+        hum={340}
+      />
+    )
+  }
+  return (
+    <Container className="mt-4">
+      <Row>
+        <Col>
+          <h2 className="mb-5"> Good Morning, Mark!</h2>
+        </Col>
+        <Col style={{ maxWidth: 'fit-content' }}>
+          <Link to="/dashboard/add">
+            <Button variant="long-sm"> Add device</Button>
+          </Link>
+        </Col>
+      </Row>
+      <div className={style.devCardParent}>{cards}</div>
+    </Container>
+  )
 }
 
 export default Home
