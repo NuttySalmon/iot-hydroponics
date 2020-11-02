333d263527ecb9b94e38d9fe6c0e5a77ac2a524d
NuttySalmon <nuttysalmon@gmail.com>
2020-11-02 03:44:46 -0800

    Redesigned navbar. Added logout

--- react-app/src/Navi/index.tsx
+++ react-app/src/Navi/index.tsx
@@ -1,48 +1,43 @@
 import React from 'react'
-import { Navbar, Nav, Dropdown, Col } from 'react-bootstrap'
+import { Navbar, Nav, Dropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Bell } from '../common/components/SvgIcons'
 import '../common/scss/components/logos.scss'
-import HeadDropdown from './HeadDropdown'
 import style from './scss/navi.module.scss'
+import HeadDropdown from './HeadDropdown'
 
 function Navi() {
   return (
-    <Navbar expand="lg" className={style.bar}>
+    <Navbar collapseOnSelect expand="lg" className={style.bar}>
       <Link to="/dashboard" className="logo-nav">
         <div className="logo logo-sm" />
       </Link>
-      <Navbar.Toggle aria-controls="navbar" />
-      <Navbar.Collapse id="navbar" className="pl-3">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Col>
-            <Link to="/dashboard" className={style.navLink}>
-              Dashboard
-            </Link>
-          </Col>
-          <Col>
-            <Link to="#notification" className={style.bell}>
+          <LinkContainer to="#notification">
+            <Nav.Link className={style.bell}>
               <Bell />
-            </Link>
-          </Col>
-          <Col>
-            <Dropdown>
-              <Dropdown.Toggle id={style.dropdown} as={HeadDropdown}>
-                <div className={style.headContain}>
-                  <img
-                    src="/img/temp-head.jpg"
-                    className={style.head}
-                    alt="User profile icon dropdown"
-                  />
-                </div>
-              </Dropdown.Toggle>
-              <Dropdown.Menu alignRight style={{ top: '1.5em', zIndex: -1 }}>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Col>
+            </Nav.Link>
+          </LinkContainer>
+
+          <Dropdown as={Nav.Link}>
+            <Dropdown.Toggle id={style.dropdown} as={HeadDropdown}>
+              <div className={style.headContain}>
+                <img
+                  src="/img/temp-head.jpg"
+                  className={style.head}
+                  alt="User profile icon dropdown"
+                />
+              </div>
+            </Dropdown.Toggle>
+            <Dropdown.Menu alignRight style={{ top: '2.5em' }}>
+              <LinkContainer to="/logout">
+                <Dropdown.Item>Logout</Dropdown.Item>
+              </LinkContainer>
+            </Dropdown.Menu>
+          </Dropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
