da7f8bcf92202699e9d9d54362b45f1a766b443a
NuttySalmon <nuttysalmon@gmail.com>
2020-11-05 23:47:07 -0800

    Minor changes to HeaderBody to allow element as header.

--- react-app/src/common/components/HeaderBody.tsx
+++ react-app/src/common/components/HeaderBody.tsx
@@ -3,8 +3,9 @@ import { Col, Container, Row } from 'react-bootstrap'
 import style from '../scss/components/header-body.module.scss'
 type HeaderBodyProps = {
   button?: React.ReactElement<any>
-  header: string
+  header: React.ReactElement<any>
 }
+
 const HeaderBody: React.FC<HeaderBodyProps> = ({
   button,
   header,
@@ -13,7 +14,7 @@ const HeaderBody: React.FC<HeaderBodyProps> = ({
   <Container className="mt-5">
     <Row className={style.headerRow}>
       <Col sm={12} md="auto">
-        <h2> {header}</h2>
+        {header}
       </Col>
       {button ? <Col className={style.headerButton}>{button}</Col> : null}
     </Row>
