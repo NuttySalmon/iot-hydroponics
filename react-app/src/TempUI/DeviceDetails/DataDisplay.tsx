18f914b4acd815d918a8d04070c78710e596c16a
NuttySalmon <nuttysalmon@gmail.com>
2020-11-15 17:50:29 -0800

    Added more device data fields for display

--- react-app/src/TempUI/DeviceDetails/DataDisplay.tsx
+++ react-app/src/TempUI/DeviceDetails/DataDisplay.tsx
@@ -4,15 +4,30 @@ import { DeviceData } from '../DeviceInfo'
 import DataBlock from './DataBlock'
 import style from './scss/deviceDetails.module.scss'
 
-
 type DataDisplayProp = Omit<DeviceData, 'status, isOnline, lastUpdatedSince'>
-const DataDisplay = ({ temp, hum, pumpOn, valveClosed }: DataDisplayProp) => {
+function getOnOff(
+  val: boolean | null,
+  trueVal = 'On',
+  falseVal = 'Off'
+): string | null {
+  return val === null ? null : val ? trueVal : falseVal
+}
+const DataDisplay = ({
+  temp,
+  hum,
+  ledOn,
+  fanOn,
+  pumpOn,
+  valveClose,
+}: DataDisplayProp) => {
   return (
     <Container fluid className={style.dataBox}>
       <DataBlock title="Temperature" data={temp} unit="&#730;C" />
       <DataBlock title="Humidity" data={hum} unit="%" />
-      <DataBlock title="Temperature" data={temp} />
-      <DataBlock title="Temperature" data={temp} />
+      <DataBlock title="LED" data={getOnOff(ledOn)} />
+      <DataBlock title="Pump" data={getOnOff(pumpOn)} />
+      <DataBlock title="Valve" data={getOnOff(valveClose, 'Close', 'Open')} />
+      <DataBlock title="Fan" data={getOnOff(fanOn)} />
     </Container>
   )
 }
--- react-app/src/TempUI/DeviceInfo.d.ts
+++ react-app/src/TempUI/DeviceInfo.d.ts
@@ -8,11 +8,10 @@ export type DeviceData = {
   temp: number | null
   hum: number | null
   lastUpdated: Date | null
-  // status: string
   isOnline: boolean
   lastUpdatedSince: string | null
   pumpOn: boolean | null
-  valveClosed: boolean | null
+  valveClose: boolean | null
+  ledOn: boolean | null
+  fanOn: boolean | null
 }
-
-export type DeviceSettings = {}
