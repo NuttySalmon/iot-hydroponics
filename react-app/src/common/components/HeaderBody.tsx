320df4e863af99c607ddfccf44b4c0a1a4d8c531
NuttySalmon <nuttysalmon@gmail.com>
2020-11-16 06:50:50 -0800

    Added back button

--- react-app/src/common/components/HeaderBody.tsx
+++ react-app/src/common/components/HeaderBody.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 import style from '../scss/components/header-body.module.scss'
 type HeaderBodyProps = {
   button?: React.ReactElement<any>
   header: React.ReactElement<any>
+  /** Display back button */
+  back?: boolean
 }
 
 const HeaderBody: React.FC<HeaderBodyProps> = ({
   button,
   header,
   children,
-}) => (
-  <Container className="pt-5 pl-5 pr-5">
-    <Row className={style.headerRow}>
-      <Col sm={12} md="auto">
-        {header}
-      </Col>
-      {button ? <Col className={style.headerButton}>{button}</Col> : null}
-    </Row>
-    <Row>{children}</Row>
-  </Container>
-)
+  back,
+}) => {
+  const history = useHistory()
+  return (
+    <>
+      {back ? (
+        <button id={style.backButton} onClick={history.goBack}>
+          &lt; Back
+        </button>
+      ) : null}
+      <Container className="pt-5 pl-5 pr-5">
+        <Row className={style.headerRow}>
+          <Col sm={12} md="auto">
+            {header}
+          </Col>
+          {button ? <Col className={style.headerButton}>{button}</Col> : null}
+        </Row>
+        <Row>{children}</Row>
+      </Container>
+    </>
+  )
+}
 
 export default HeaderBody
