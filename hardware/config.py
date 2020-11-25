bc083a16f92c49128e6ad6688894cb9fa3fe2344
NuttySalmon <nuttysalmon@gmail.com>
2020-11-24 17:04:40 -0800

    Remove config file from source control

--- hardware/config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class InventoryItem:
-    '''Class for keeping track of an item in inventory.'''
-    name: str
-    unit_price: float
-    quantity_on_hand: int = 0
