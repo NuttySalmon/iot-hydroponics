90091f2d3ceab1ecb1b1775fc65a5b37ee7075e1
NuttySalmon <nuttysalmon@gmail.com>
2020-11-29 20:47:22 -0800

    Restructure hydroponics class in own file.

--- hardware/main.py
+++ hardware/main.py
@@ -1,87 +1,10 @@
-from behaviour import Behaviour
-import json
 from config import IOT_CONFIG
-from aws_iot_client import IoTClient
-from threading import Event, Thread
-from data import Data
-from settings import Settings
-from time import sleep
+from hydroponics import Hydroponics
 
 
-PUBLISH_INTERVAL = 60
-
-
-def run_task_interval(interval=1):
-    def decorator(function):
-        def wrapper(*args, **kwargs):
-            while 1:
-                result = function(*args, **kwargs)
-                sleep(interval)
-            return result
-
-        return wrapper
-
-    return decorator
-
-
-class Hydroponics:
-    def __init__(self, iot_config):
-        self.behaviour = Behaviour()
-        topic_prefix = "iothydroponics/device/{}".format(IOT_CONFIG.client_id)
-        settings_topic = "{}/settings".format(topic_prefix)
-        device_update_topic = "{}/update".format(topic_prefix)
-        self.iot_config = iot_config
-        self.client = IoTClient(
-            settings_topic=settings_topic,
-            device_update_topic=device_update_topic,
-            **self.iot_config.__dict__
-        )
-        self.publish_thread = Thread(target=self.publish_at_interval)
-
-    @property
-    def current_settings(self):
-        return self.behaviour.settings
-    
-    @property
-    def current_data(self):
-        return self.behaviour.data
-
-    @run_task_interval(0.5)
-    def publish_at_interval(self):
-        self.publish()
-
-    @run_task_interval(0.5)
-    def timed_events(self):
-        self.behaviour.update_all()
-        
-    def publish(self):
-        # print(
-        #     "Publish: \n data:{}\n settings:{}\n".format(
-        #         self.current_data.__dict__, self.current_settings.__dict__
-        #     )
-        # )
-        self.client.publish_update(self.current_data, self.current_settings)
-
-    # Callback when the subscribed topic receives a message
-
-    def on_message_received(self, topic, payload, **kwargs):
-        new_settings = json.loads(payload)
-        print("New settings: {}\n".format(new_settings))
-        self.behaviour.update_settings(new_settings)
-        self.publish()  # republish immediately
-
-    def start(self):
-        self.client.subscribe_topic(self.on_message_received)
-        self.iot_client_run_thread = Thread(target=self.client.run)
-        self.edge_behaviour = Thread(target=self.timed_events)
-        self.iot_client_run_thread.start()
-        self.publish_thread.start()
-        self.edge_behaviour.start()
 
 
 if __name__ == "__main__":
      
-    
     instance = Hydroponics(IOT_CONFIG)
-    # instance.behaviour._peri.led.colorWipe((255, 0, 255))
     instance.start()
